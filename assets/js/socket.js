c6c6f57dea994eab589fbb4a72f276c734b523ad
jackrc7 <cardin.j@husky.neu.edu>
2021-02-24 18:09:23 -0500

    first

---
+++ assets/js/socket.js
@@ -0,0 +1,73 @@
+import {Socket} from "phoenix";
+
+let socket = new Socket(
+  "/socket",
+  {params: {token: ""}}
+);
+socket.connect();
+
+let state = {
+  word: "______",
+  guesses: [],
+  name: ""
+};
+
+// console.log("name-1:");
+console.log(state.name);
+let channel = socket.channel("game:1", {}); // name goes here FIXME THIS IS THE PROBLEM
+
+// console.log("name-2:");
+// console.log(state.name);
+// console.log("channel:")
+
+
+let callback = null;
+
+// The server sent us a new state.
+function state_update(st) {
+  console.log("New state", st);
+  state = st;
+  if (callback) {
+    callback(st);
+  }
+}
+
+export function ch_join(cb) {
+  callback = cb;
+  callback(state);
+}
+
+export function ch_login(name) {
+  // channel = socket.channel("game:" + name, {}); // name goes here FIXME
+  console.log("channel"  + name);
+  channel.push("login", {name: name})
+         .receive("ok", state_update)
+         .receive("error", resp => {
+           console.log("Unable to login", resp)
+         });
+  
+}
+
+export function ch_push(guess) {
+  channel.push("guess", guess)
+         .receive("ok", state_update)
+         .receive("error", resp => {
+           console.log("Unable to push", resp)
+         });
+}
+
+export function ch_reset() {
+  channel.push("reset", {})
+         .receive("ok", state_update)
+         .receive("error", resp => {
+           console.log("Unable to push", resp)
+         });
+}
+
+channel.join()
+       .receive("ok", state_update)
+       .receive("error", resp => {
+         console.log("Unable to join", resp)
+       });
+
+channel.on("view", state_update);
